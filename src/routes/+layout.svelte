114ec9c1c0e040bbc750d8f0782d3d67d557c19c
Delfi-CH <delfi@delfi.dev>
2026-06-11 09:54:43 +0200

    feat: move to sveltestrap for styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	//import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import Navigation from '$lib/components/Navigation.svelte';
+	import "bootswatch/dist/solar/bootstrap.min.css"
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Navigation></Navigation>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
-	import DownloadInterface from '$lib/components/GenericDownloadInterface.svelte';
-	import { Modloader, ModloaderType } from '$lib/serverManager';
-	import { onMount } from 'svelte';
 
-	onMount(async () => {
-		const ml = new Modloader(ModloaderType.Vanilla, '1.21.1');
-		await ml.buildURL();
-	});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<DownloadInterface
-	downloadURL="https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.tar.gz"
-	downloadPath="/tmp"
-	downloadFilename="jdk21-linux.tar.gz"
-></DownloadInterface>
+<h1>minopanel</h1>
